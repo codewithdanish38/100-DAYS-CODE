ad745c0a6387220f3d7a00556769ab61845a9245
codewithdanish38 <khanlko2002@gmail.com>
2024-10-30 21:55:35 +0530

    Reverse A Array

---
+++ src/Arrays/Sort_Zero_One_Two.java
@@ -0,0 +1,41 @@
+package Arrays;
+
+public class Sort_Zero_One_Two {
+    public static void Printarray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void Sort(int arr[]) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr, mid, low);
+                mid++;
+                low++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else { // arr[mid] == 2
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {0, 1, 0, 2, 0, 1};
+        Sort(arr);
+        Printarray(arr);
+    }
+}
