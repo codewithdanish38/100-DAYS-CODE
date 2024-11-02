94ddf7da6ae106c80fc0f5e7554756d7e1b234dd
codewithdanish38 <khanlko2002@gmail.com>
2024-11-02 15:59:24 +0530

    Kth-Maximum

--- src/Arrays/Kth_max_min.java
+++ src/Arrays/Kth_max_min.java
@@ -4,38 +4,65 @@ import java.util.Scanner;
 
 public class Kth_max_min {
 
-    public static void Printarray(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
-    public static int Kth_Max(int arr[],int k) {
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<k;i++){
-            max=Integer.MIN_VALUE;
-            int MaxIndex=-1;
-            for(int j=0;j<arr.length;j++){
-                if(arr[j]>max){
-                    max=arr[j];
-                    MaxIndex=j;
+
+    public static int kthMax(int arr[], int k) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < k; i++) {
+            max = Integer.MIN_VALUE;
+            int maxIndex = -1;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                    maxIndex = j;
                 }
             }
-            if(arr[MaxIndex]!=-1){
-                arr[MaxIndex]=Integer.MIN_VALUE;
+            if (maxIndex != -1) {
+                arr[maxIndex] = Integer.MIN_VALUE;
             }
         }
         return max;
     }
 
+    public static int kthMin(int arr[], int k) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < k; i++) {
+            min = Integer.MAX_VALUE;
+            int minIndex = -1;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            if (minIndex != -1) {
+                arr[minIndex] = Integer.MAX_VALUE;
+            }
+        }
+        return min;
+    }
 
     public static void main(String[] args) {
-        int arr[]={1,4,5,10,11,76};
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the value of k");
-        int k=sc.nextInt();
-         int result=Kth_Max(arr,k);
-        System.out.println("The Kth largest element of array:"+result);
+        int arr[] = {1, 4, 5, 10, 11, 76};
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter the value of k:");
+        int k = sc.nextInt();
+
+        System.out.print("Original array: ");
+        printArray(arr);
+
+        int maxResult = kthMax(arr.clone(), k);  // Cloning array to keep it unchanged
+        System.out.println("The " + k + "-th largest element of the array: " + maxResult);
+
+        int minResult = kthMin(arr.clone(), k);  // Cloning array to keep it unchanged
+        System.out.println("The " + k + "-th smallest element of the array: " + minResult);
+
+        sc.close();
     }
 }
